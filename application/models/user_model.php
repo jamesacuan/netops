f497535e0148356565f0dbd0f0ac912060f8d643
James Earl C. Sacuan <jecsacuan@sencorhq.net>
2017-05-22 18:08:01 +0800

    revert back to exp

--- application/models/user_model.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('BASEPATH')) exit ('No direct script access allowed');
-
-class user_model extends CI_Model{
-    public function getProfile($id){
-        $query=$this->db->get('users');
-        return $query->result();
-    }
-}
