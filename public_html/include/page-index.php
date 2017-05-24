7e422c12c4931971bdd68a119e0c2bcf4060938d
jamesacuan <jec@sacuan.com>
2017-05-24 17:16:42 +0800

    removed names

--- public_html/include/page-index.php
+++ public_html/include/page-index.php
@@ -16,133 +16,31 @@
               <th class="">Third Shift</th>
             </tr>
             <tr>
-              <td class="">Rommel Adio</td>
-              <td class="">Enrique Reyes</td>
-              <td class="">John M. Vinluan</td>
-            </tr>
-            <tr>
-              <td class="">Ray Joseph Quintal</td>
-              <td class="">Glenn Carlo Arocha</td>
-              <td class="">Crisanto Tabuso</td>
-            </tr>
-            <tr>
-              <td class="">Mark Anthony M. Sapitan</td>
-              <td class="">Jezreel R. Catap</td>
+              <td class=""></td>
+              <td class=""></td>
               <td class=""></td>
             </tr>
             <tr>
-              <td class="">James Earl Sacuan</td>
-              <td class="">Kervin Marilag</td>
+              <td class=""></td>
+              <td class=""></td>
               <td class=""></td>
             </tr>
             <tr>
-              <td>Jefferson Yadao</td>
+              <td class=""></td>
+              <td class=""></td>
+              <td class=""></td>
+            </tr>
+            <tr>
+              <td class=""></td>
+              <td class=""></td>
+              <td class=""></td>
+            </tr>
+            <tr>
+              <td></td>
             </tr>
           </table>
         </div>
       </div>
-      <div class="box crew clear col-60 left">
-        <h3>Team</h3>
-        <div>
-          <div class="block primary">
-            <div class="pic">
-            </div>
-            <a>Regino B. Resurrecion</a>
-            <span>Manager</span>
-            <span>ricky.resurrecion@sencor.net</span>
-          </div>
-          <div class="block primary">
-            <div class="pic">
-            </div>
-            <a>Raymundo L. Untaran</a>
-            <span>Level 3, Network Administrator</span>
-            <span>raymund.untaran@sencor.net</span>
-          </div>
-         <div class="clear"></div>
-          <div class="block">
-            <div class="pic" style="">
-              <img src="assets/block-johnv.jpg" />
-            </div>
-
-            <a>John Vinluan</a>
-            <span>Level 2, Network Administrator</span>
-            <span>john.vinluan@sencor.net</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-            </div>
-            <a>Rommel Adio</a>
-            <span>Level 2, Network Administrator</span>
-            <span>rommel.adio@sencor.net</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-            </div>
-            <a>Enrique Reyes</a>
-            <span>Level 2, Network Administrator</span>
-            <span>kier.reyes@sencor.net</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-rj.jpg" />
-            </div>
-            <a>Ray Joseph Quintal</a>
-            <span>Level 1, Network Administrator</span>
-            <span>rj.quintal@sencor.net</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-glenn.jpg" />
-            </div>
-            <a>Glenn Carlo Arocha</a>
-            <span>Level 1, Network Administrator</span>
-            <span>glenn.arocha@sencor.net</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-jez.jpg" />
-            </div>
-            <a>Jezreel Catap</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-            </div>
-            <a>Crisanto Tabuso</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-marks.jpg" />
-            </div>
-            <a>Mark Sapitan</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-ken.jpg" />
-            </div>
-            <a>Kervin Marilag</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-            </div>
-            <a>James Sacuan</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-              <img src="assets/block-jeff.jpg" />
-            </div>
-            <a>Jefferson Yadao</a>
-            <span>Level 1, Network Administrator</span>
-          </div>
-          <div class="block">
-            <div class="pic">
-            </div>
-            <a>Enriquito Loya</a>
-            <span>Network Administrator</span>
-          </div>
-        </div>
-      </div>
+    </div>
+  </div>
+</div>
