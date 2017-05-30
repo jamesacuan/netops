6c002e1b202c47a6c18fd76bbd3032e471de8794
jamesacuan <jec@sacuan.com>
2017-05-30 16:15:46 +0800

    applying oop to net scan

--- script/ping.py
+++ script/ping.py
@@ -1,138 +1,176 @@
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox as tm
 import MySQLdb
 import os
 
-root = Tk()
-
-  
-# Fetch a single row using fetchone() method.
-#data = cursor.fetchone()
-
-#print ("Database version : %s " % data)
-
-# disconnect from server
-
-def donothing():
-   filewin = Toplevel(root)
-   button = Button(filewin, text="Do nothing button")
-   button.pack()
-
-def showConfigureDatabase():
-    windb = Toplevel(root)
-    lbl1  = Label(windb, text="MySQL Directory")
-    lbl2  = Label(windb, text="Username")
-    lbl3  = Label(windb, text="Password")
-    ent1  = Entry(windb)
-    ent2  = Entry(windb)
-    ent3  = Entry(windb)
-    btn1  = Button(windb, text="Save")
-    btn2  = Button(windb, text="Test connection")
-
-    lbl1.grid(row=0, sticky=E)
-    lbl2.grid(row=1, sticky=E)
-    lbl3.grid(row=2, sticky=E)
-
-    btn1.grid(row=3)
-    btn2.grid(row=3, column=1)
-
-    ent1.insert(0, "C:\\xampp\\mysql\\bin\\")
-    ent2.insert(0, "root")
-    ent1.grid(row=0, column=1)
-    ent2.grid(row=1, column=1)
-    ent3.grid(row=2, column=1)
-
-def openSelection(evt):
-   value   = str((Lb1.get(ACTIVE)))
-   winhost = Toplevel(root)
-   winhost.title(value)
-   lbl1    = Label(winhost, text="IP Address")
-   lbl2    = Label(winhost, text="MAC Address")
-   lbl3    = Label(winhost, text="Hostname")
-   lbl4    = Label(winhost, text="Operating System")
-
-   ent1    = Entry(winhost)
-   ent2    = Entry(winhost)
-   ent3    = Entry(winhost)
-   ent4    = Entry(winhost)
-
-   lbl1.grid(row=0, sticky=E)
-   lbl2.grid(row=1, sticky=E)
-   lbl3.grid(row=2, sticky=E)
-   lbl4.grid(row=3, sticky=E)
-
-   ent1.insert(0, "")
-   ent2.insert(0, "")
-   ent3.insert(0, value)
-   ent4.insert(0, "")
-
-   ent1.grid(row=0, column=1)
-   ent2.grid(row=1, column=1)
-   ent3.grid(row=2, column=1)
-   ent4.grid(row=3, column=1)
+class Ping(tk.Tk):
    
-def quit():
-    root.destroy()
-   
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Scan...", command=donothing)
+   def __init__(self, *args, **kwargs):
+      tk.Tk.__init__(self, *args, **kwargs)
+      
+      tk.Tk.wm_title(self, "Ping")
 
-filemenu.add_separator()
-filemenu.add_command(label="Configure database...", command=showConfigureDatabase)
-filemenu.add_command(label="Exit", command=quit)
-menubar.add_cascade(label="File", menu=filemenu)
-editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Undo", command=donothing)
+      container = tk.Frame(self)
+      container.pack(side="top", fill="both", expand=True)
+      container.grid_rowconfigure(0, weight=1)
+      container.grid_columnconfigure(0, weight=1)
 
-editmenu.add_separator()
+      self.frames = {}
 
+      for F in (MainPage, ConfigDBPage, Login):
+         frame = F(container, self)
+         self.frames[F] = frame
+         frame.grid(row=0, column=0, sticky="nsew")
 
-menubar.add_cascade(label="Edit", menu=editmenu)
+      self.show_frame(Login)
 
-# TOOLBAR
+   def show_frame(self, cont):
+      frame = self.frames[cont]
+      frame.tkraise()
 
-toolbar = Frame(root)
-toolbar.pack(side=TOP, fill=X)
+      menubar = frame.menubar(self)
+      self.configure(menu=menubar)
 
-button1 = Button(toolbar, text="Scan", command=donothing)
-button1.pack(side=LEFT, padx=5, pady=2)
+   def Login_onClick(param, param2):
+      print(param + " " + param2)
 
-# STATUSBAR
+   def quit():
+      self.destroy()
 
-status = Label(root, text="status...", bd=1, relief=SUNKEN, anchor=W)
-status.pack(side=BOTTOM, fill=X)
+   def openSelection(evt):
+      value   = str((Lb1.get(ACTIVE)))
+      winhost = Toplevel(root)
+      winhost.title(value)
+      lbl1    = Label(winhost, text="IP Address")
+      lbl2    = Label(winhost, text="MAC Address")
+      lbl3    = Label(winhost, text="Hostname")
+      lbl4    = Label(winhost, text="Operating System")
 
-# BODY
-Sb1 = Scrollbar(root)
-Sb1.pack(side=RIGHT, fill=Y)
+      ent1    = Entry(winhost)
+      ent2    = Entry(winhost)
+      ent3    = Entry(winhost)
+      ent4    = Entry(winhost)
 
-Lb1 = Listbox(root, yscrollcommand=Sb1.set)
+      lbl1.grid(row=0, sticky=E)
+      lbl2.grid(row=1, sticky=E)
+      lbl3.grid(row=2, sticky=E)
+      lbl4.grid(row=3, sticky=E)
 
-db = MySQLdb.connect("localhost", "root", "", "db_netops")
-cursor = db.cursor()
+      ent1.insert(0, "")
+      ent2.insert(0, "")
+      ent3.insert(0, value)
+      ent4.insert(0, "")
 
-sql = "SELECT * FROM `workstation` order by hostname"
-try:
-   cursor.execute(sql)
-   results = cursor.fetchall()
-   for row in results:
-      hostid = row[0]
-      hostname = row[1]
-      hostip = row[2]
-      hostmac = row[3]
-      Lb1.insert(row[0], hostname)
-   
-except:
-   print ("Error: unable to fecth data")
+      ent1.grid(row=0, column=1)
+      ent2.grid(row=1, column=1)
+      ent3.grid(row=2, column=1)
+      ent4.grid(row=3, column=1)
 
-db.close()
+class MainPage(tk.Frame):
 
-Lb1.bind("<Double-Button-1>", openSelection)
-Lb1.pack(expand=1,fill=BOTH)
+   def __init__(self, parent, controller):
+      tk.Frame.__init__(self, parent)
+      #label = tk.Label(self, text="Start Page")
+      #label.pack(pady=10, padx=10)
+     
+      toolbar = tk.Frame(self)
+      toolbar.pack(side=tk.TOP, fill=tk.X)
 
-Sb1.config(command=Lb1.yview)
+      button1 = ttk.Button(toolbar, text="Scan", command=quit)
+      button1.pack(side=tk.LEFT, padx=5, pady=2)
 
-root.title("Ping")
-root.geometry("300x500")
-root.config(menu=menubar)
-root.mainloop()
+      #status bar
+      status = tk.Label(self, text="status...", bd=1, relief=tk.SUNKEN, anchor=tk.W)
+      status.pack(side=tk.BOTTOM, fill=tk.X)
+
+      #body
+      Sb1 = tk.Scrollbar(self)
+      Sb1.pack(side=tk.RIGHT, fill=tk.Y)
+
+      Lb1 = tk.Listbox(self, yscrollcommand=Sb1.set)
+      Lb1.pack(expand=1,fill=tk.BOTH)
+
+      db = MySQLdb.connect("localhost", "root", "", "db_netops")
+      cursor = db.cursor()
+
+      sql = "SELECT * FROM `workstation` order by hostname"
+      try:
+         cursor.execute(sql)
+         results = cursor.fetchall()
+         for row in results:
+            hostid = row[0]
+            hostname = row[1]
+            hostip = row[2]
+            hostmac = row[3]
+            Lb1.insert(row[0], hostname)
+         
+      except:
+         print ("Error: unable to fecth data")
+
+      db.close()
+
+      Sb1.config(command=Lb1.yview)
+
+   def menubar(self, root):
+      menubar  = tk.Menu(self)
+      filemenu = tk.Menu(menubar, tearoff=0)
+      filemenu.add_command(label="Scan...", command=quit)
+      filemenu.add_separator()
+      filemenu.add_command(label="Configure database...", command=lambda: controller.show_frame(ConfigDBPage))
+      filemenu.add_command(label="Exit", command=quit)
+      menubar.add_cascade(label="File", menu=filemenu)
+
+      editmenu = tk.Menu(menubar, tearoff=0)
+      editmenu.add_command(label="Login", command=quit)
+      editmenu.add_separator()
+      menubar.add_cascade(label="Edit", menu=editmenu)
+
+      return menubar
+
+class Login(tk.Frame):
+
+   def __init__(self, parent, controller):
+      tk.Frame.__init__(self, parent)
+      label_1  = tk.Label(self, text="Username")
+      label_2  = tk.Label(self, text="Password")
+      entry_1  = tk.Entry(self)
+      entry_2  = tk.Entry(self, show="*")
+      
+      loginbtn = ttk.Button(self, text="Login",command=lambda: controller.show_frame(MainPage))
+                           #command=lambda: Login_onClick(entry_1.get(), entry_2.get())
+      
+      label_1.grid(row=0, sticky="E")
+      label_2.grid(row=1, sticky="E")
+      entry_1.grid(row=0, column=1)
+      entry_2.grid(row=1, column=1)
+      loginbtn.grid(columnspan=2)
+
+class ConfigDBPage(tk.Frame):
+
+   def __init__(self, parent, controller):
+      tk.Frame.__init__(self, parent)
+      lbl1  = tk.Label(self, text="MySQL Directory")
+      lbl2  = tk.Label(self, text="Username")
+      lbl3  = tk.Label(self, text="Password")
+      ent1  = tk.Entry(self)
+      ent2  = tk.Entry(self)
+      ent3  = tk.Entry(self)
+      btn1  = tk.Button(self, text="Save")
+      btn2  = tk.Button(self, text="Test connection")
+
+      lbl1.grid(row=0, sticky=tk.E)
+      lbl2.grid(row=1, sticky=tk.E)
+      lbl3.grid(row=2, sticky=tk.E)
+
+      btn1.grid(row=3)
+      btn2.grid(row=3, column=1)
+
+      ent1.insert(0, "C:\\xampp\\mysql\\bin\\")
+      ent2.insert(0, "root")
+      ent1.grid(row=0, column=1)
+      ent2.grid(row=1, column=1)
+      ent3.grid(row=2, column=1)
+       
+app = Ping()
+app.mainloop()
